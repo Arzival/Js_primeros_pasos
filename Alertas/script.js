41be01f31d3cf8f4426c7b0ff61a6bd7128d3e31
Arzival <arzival.sistemas@gmail.com>
2020-09-11 03:23:57 -0500

    ejercicio con alertas

---
+++ Alertas/script.js
@@ -0,0 +1,10 @@
+//alerta
+alert("hola mundo");
+
+//confirmacion
+var resultado = confirm("seguro de tus datos");
+console.log(resultado);
+
+//ingresar datos
+var edad = prompt("tu edad",0); //regresa un String
+console.log(edad);
