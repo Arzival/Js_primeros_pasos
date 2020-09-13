a8b38c6ab101d71029a518cf6951484b6dedb9ff
Arzival <arzival.sistemas@gmail.com>
2020-09-12 20:46:20 -0500

    terminado el tema de textos

--- TextosJQuery/funciones.js
+++ TextosJQuery/funciones.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-
+  reloadLinks();
+  $("#add_button").click(function () {
+    $("#lista").prepend('<li><a href="' + $("#add_link").val() + '"></a></li>');
+    reloadLinks();
+  });
 });
+
+function reloadLinks() {
+  $("a").each(function (index) {
+    //each me ayuda a recorrer un elemento
+    var that = $(this);
+    var enlace = that.attr("href"); //attr me ayuda a obtener el texto que tiene dentro mi elemento
+    that.attr('target','_blank')
+    that.text(enlace);
+  });
+}
