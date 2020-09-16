17dd8d3ae050ac079bc4cfcf590d4b9eab216566
Arzival <arzival.sistemas@gmail.com>
2020-09-16 00:08:45 -0500

    peticiones

---
+++ Ajax/funciones.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  // load
+  //$("#datos").load("https://reqres.in/");
+  //get y post
+  $.get("https://reqres.in/api/users", { page: 2 }, function (response) {
+    console.log(response);
+    response.data.forEach((element, index) => {
+      $("#datos").append("<p>" + element.first_name + "</p>");
+    });
+  });
+
+  $("#formulario").submit(function (e) {
+    e.preventDefault();
+    var url = $(this).attr("action");
+    var usuario = {
+      name: $('input[name="user"]').val(),
+      web: $('input[name="web"]').val(),
+    };
+
+    // $.post(url, usuario, function (response) {
+    //   console.log(response);
+    // }).done(function () {
+    //   alert("user regustradi");
+    // });
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: usuario,
+        success: function (response) {
+            console.log(response);
+        }
+    });
+    return false;
+  });
+});
