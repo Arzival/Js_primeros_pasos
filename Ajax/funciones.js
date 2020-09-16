61ad5f67f2e65401021a2f7da2a6c07a7da32354
Arzival <arzival.sistemas@gmail.com>
2020-09-16 00:26:12 -0500

    peticiones

--- Ajax/funciones.js
+++ Ajax/funciones.js
@@ -2,12 +2,22 @@ $(document).ready(function () {
   // load
   //$("#datos").load("https://reqres.in/");
   //get y post
-  $.get("https://reqres.in/api/users", { page: 2 }, function (response) {
-    console.log(response);
-    response.data.forEach((element, index) => {
-      $("#datos").append("<p>" + element.first_name + "</p>");
-    });
-  });
+  // $.get("https://reqres.in/api/users", { page: 2 }, function (response) {
+  //   console.log(response);
+  //   response.data.forEach((element, index) => {
+  //     $("#datos").append("<p>" + element.first_name + "</p>");
+  //   });
+  // });
+  $.ajax({
+    type: "GET",
+    url:"https://reqres.in/api/users?page=2",
+    success:  (response) => {
+        console.log(response);
+        response.data.forEach(element => {
+          $("#datos").append("<p>" + element.first_name + "</p>");
+        });
+    }
+});
 
   $("#formulario").submit(function (e) {
     e.preventDefault();
@@ -22,11 +32,12 @@ $(document).ready(function () {
     // }).done(function () {
     //   alert("user regustradi");
     // });
+    
     $.ajax({
         type: "POST",
         url: url,
         data: usuario,
-        success: function (response) {
+        success:(response)=> {
             console.log(response);
         }
     });
